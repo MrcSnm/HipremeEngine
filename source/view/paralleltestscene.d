bc25bb14846267a9eff022797e10d5a677c4324a
Hipreme <msnmancini@hotmail.com>
2021-07-26 13:39:29 -0300

    Fixed assetpacker error

--- source/view/paralleltestscene.d
+++ source/view/paralleltestscene.d
@@ -1,4 +1,5 @@
 module view.paralleltestscene;
+import data.assetpacker;
 import data.assetmanager;
 import view.scene;
 
@@ -6,7 +7,16 @@ class ParallelTestScene : Scene
 {
     this()
     {
-        HipAssetManager.loadImage("./assets/graphics/sprites/rex.png", true);
+        // HipAssetManager.loadImage("./assets/graphics/sprites/rex.png", true);
+
+        updateAssetInPack("gamepack.hap", 
+        [
+            "assets/audio/active_matrix_edited.wav",
+            "assets/audio/junkyard-a-class.mp3",
+            "assets/audio/the-sound-of-silence.wav",
+            "nodub.sh",
+        ]);
+        
         
     }
     override void render()
