5bb0496ea4b6532b37d303699d378c5b760a49be
Hipreme <msnmancini@hotmail.com>
2021-08-16 09:00:37 -0300

    Remove useless and heavy files from android

--- build/android/project/app/src/main/java/com/hipremeengine/app/HipremeEngine.java
+++ build/android/project/app/src/main/java/com/hipremeengine/app/HipremeEngine.java
@@ -1,27 +1,17 @@
 package com.hipremeengine.app;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
 import android.content.res.Resources;
-import android.opengl.GLSurfaceView;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.util.DisplayMetrics;
 import android.util.Log;
-import android.view.View;
-
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.Window;
-import android.view.WindowInsetsController;
 import android.view.WindowManager;
+
+import java.io.IOException;
 
 public class HipremeEngine extends Activity
 {
@@ -33,6 +23,9 @@ public class HipremeEngine extends Activity
     public static boolean isRunning = true;
     public static Thread gameThread;
     public static HipremeEngine instance;
+    ///Initializes the D runtime
+    public static AssetManager assetManager;
+    public static native void HipremeInit();
     public static native int HipremeMain();
     public static native boolean HipremeUpdate();
     public static native void HipremeRender();
@@ -45,7 +38,15 @@ public class HipremeEngine extends Activity
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        try {
+            for(String s : getApplicationContext().getResources().getAssets().list(""))
+                System.out.println(s);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assetManager = getApplicationContext().getResources().getAssets();
         instance = this;
+
         view = new HipSurfaceView((this));
         setContentView(view);
     }
@@ -53,6 +54,7 @@ public class HipremeEngine extends Activity
     protected void onDestroy()
     {
         super.onDestroy();
+        HipremeDestroy();
         HipremeEngine.instance = null;
     }
 
@@ -60,6 +62,11 @@ public class HipremeEngine extends Activity
     {
         DisplayMetrics m = Resources.getSystem().getDisplayMetrics();
         return new int[]{m.widthPixels, m.heightPixels};
+    }
+    public static Object getAssetManager()
+    {
+        System.out.println("OPA AMIGAO");
+        return assetManager;
     }
 
     public static String getApplicationDir() throws PackageManager.NameNotFoundException
