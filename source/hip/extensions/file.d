d010332ffe3be8cb08eebdc441e310f173c8a448
Marcelo Silva Nascimento Mancini <msnmancini@hotmail.com>
2022-07-13 14:46:00 -0300

    Create file.d
    In future this will be separated for each type (somehow)
    
    Former-commit-id: e8b5e8482010edced7675f2b282a4ee72da0f41e
    Former-commit-id: d80e36641977cc4c5e6823738b70306d64311c39 [formerly 4f4f2625025a775381e88342e55fd543e5ceaf8d]
    Former-commit-id: e1008a927f6d5b9f797c4f3246f3ee5c30898e4e

---
+++ source/hip/extensions/file.d
@@ -0,0 +1,63 @@
+module hip.extensions.file;
+import hip.filesystem.hipfs;
+
+
+//Definitions
+import hip.data.assets.image;
+bool loadFromFile(Image image, string path)
+{
+    ubyte[] data;
+    if(!HipFS.read(path, data))
+        return false;
+    return image.loadFromMemory(data);
+}
+
+import hip.hiprenderer.texture;
+bool loadFromFile(Texture texture, string path)
+{
+    ubyte[] data;
+    if(!HipFS.read(path, data))
+        return false;
+    return texture.load(null);
+}
+
+
+import hip.font.bmfont;
+bool loadFromFile(HipBitmapFont font, string atlasPath)
+{
+    ubyte[] data;
+    if(!HipFS.read(atlasPath, data))
+        return false;
+    font.readAtlas(atlasPath);
+    font.readTexture();
+    return true;
+}
+
+import hip.font.ttf;
+bool loadFromFile(Hip_TTF_Font font, string path)
+{
+    ubyte[] data;
+    if(!HipFS.read(path, data))
+        return false;
+    return font.loadFromMemory(data);
+}
+
+import hip.data.ini;
+bool loadFromFile(out IniFile ini, string path)
+{
+    string data;
+    if(!HipFS.readText(path, data))
+        return false;
+    ini = IniFile.parse(cast(string)data);
+    return ini.noError;
+}
+
+import hip.data.assetpacker;
+bool loadFromFile(HapFile hapFile, string path)
+{
+    void[] data;
+    if(!HipFS.read(path, data))
+        return false;
+    hapFile = new HapFile(data);
+    return true;
+}
