81ba87c6ada7e1144575e253ea259ea8b4be3a7e
Marcelo Silva Nascimento Mancini <msnmancini@hotmail.com>
2022-07-13 12:17:12 -0300

    Sending bmfont to the font module
    Sending bmfont to the font module
    
    Former-commit-id: 5d71446ca40dc4c8eb35ad8ea3c1ecc1866ff353
    Former-commit-id: 6c49b82a90a862546e127bd55b38fda40dfaa2b0 [formerly fe53c072ad715293278cc06201be52138fd49bbb]
    Former-commit-id: 9348d302d4c89836548739421ac196951edb50f0

--- source/hip/graphics/g2d/textrenderer.d
+++ source/hip/graphics/g2d/textrenderer.d
@@ -8,7 +8,7 @@ Distributed under the CC BY-4.0 License.
    (See accompanying file LICENSE.txt or copy at
 	https://creativecommons.org/licenses/by/4.0/
 */
-module hip.graphics.g2d.bitmaptext;
+module hip.graphics.g2d.textrenderer;
 import hip.graphics.g2d;
 import hip.graphics.mesh;
 import hip.util.data_structures;
@@ -30,171 +30,11 @@ enum HipTextAlign
     BOTTOM
 }
 
-class HipBitmapFont : HipFont
-{
-    Texture texture;
-    ///The atlas path is saved inside the class
-    string atlasPath;
-    ///This variable is defined when the atlas is being read
-    string atlasTexturePath;
-
-    ///Use that property to know how many characters was read inside the atlas
-    uint charactersCount;
-
-
-    HipFontKerning kerning;
-    // Pair!(int, int)[][int] kerning;
-
-    void readAtlas(string atlasPath)
-    {
-        import core.stdc.string : strlen;
-        import core.stdc.stdio;
-        this.atlasPath = atlasPath;
-        char[512] name;
-        int size;
-
-        int bold, italic;
-        char[512] charset;
-        int unicode;
-        int stretchH;
-        int smooth, aa;
-
-        int paddingX, paddingY, paddingW, paddingH;
-        int spacingX, spacingY;
-        int outline;
-
-        //Common
-        int lineHeight, base, scaleW, scaleH, pages, packed, alpha, red, green,blue;
-        //Page
-        int pageId;
-        char[512] file;
-        //chars
-        int count;
-
-
-        FILE* f = fopen(atlasPath.ptr, "r");
-        if(f == null)
-        {
-            logln("Error! Could not read file ", atlasPath, "!!\n\n");
-            return;
-        }
-
-        //Doing that disables the warning
-        string format = "info face=\"%[^\"]%512s size=%d bold=%d italic=%d charset=%512s unicode=%d stretchH=%d smooth=%d aa=%d padding=%d,%d,%d,%d spacing=%d,%d outline=%d\n";
-        fscanf(f, format.ptr,
-        name.ptr, &size, &bold, &italic, charset.ptr, &unicode, &stretchH, &smooth, &aa,
-        &paddingX, &paddingY, &paddingW, &paddingH, &spacingX, &spacingY, &outline);
-
-        //Common
-        fscanf(f, "common lineHeight=%d base=%d scaleW=%d scaleH=%d pages=%d packed=%d alphaChnl=%d redChnl=%d greenChnl=%d blueChnl=%d\n",
-        &lineHeight, &base, &scaleW, &scaleH, &pages, &packed, &alpha, &red, &green, &blue);
-
-        //Page
-        format = "page id=%d file=\"%[^\"]%512s\n";
-        fscanf(f, format.ptr, &pageId, file.ptr);
-
-        atlasTexturePath = (cast(string)file)[0..strlen(file.ptr)];
-        //Count
-        fscanf(f, "chars count=%d\n", &count);
-
-
-        
-        size_t memSize = (count > 256 ? count : 256);
-
-        scope HipFontChar[] ch = new HipFontChar[memSize];
-        charactersCount = count;
-
-        uint maxWidth = 0;
-        for(int i = 0; i < count; i++)
-        {
-            HipFontChar c;
-            fscanf(f, "char id=%u x=%d y=%d width=%d height=%d xoffset=%d yoffset=%d xadvance=%d page=%d chnl=%d\n",
-            &c.id, &c.x, &c.y, &c.width, &c.height, &c.xoffset, &c.yoffset, &c.xadvance, &c.page, &c.chnl);
-            ch[c.id] = c;
-            
-            maxWidth = max(maxWidth, c.width);
-        }
-        if(characters[' '].width == 0 && characters[' '].xadvance == 0)
-            spaceWidth = maxWidth;
-        else
-            spaceWidth = characters[' '].xadvance;
-
-        lineBreakHeight = lineHeight;
-        
-        int kerningCount = 0;
-
-        fscanf(f, "kernings count=%d\n", &kerningCount);
-        
-        int kFirst, kSecond, kValue;
-        for(int i = 0; i < kerningCount; i++)
-        {
-            fscanf(f, "kerning first=%d second=%d amount=%d\n", &kFirst, &kSecond, &kValue);
-            if((kFirst in kerning) is null)
-                kerning[kFirst] = HipCharKerning.init;
-            kerning[kFirst][kSecond] = kValue;
-        }
-        fclose(f);
-        foreach(HipFontChar hChar; ch)
-            characters[hChar.id] = hChar;
-        
-
-    }
-
-    void readTexture(string texturePath = "")
-    {
-        import hip.util.string : lastIndexOf;
-        if(texturePath == "" && atlasTexturePath != "")
-        {
-            const long ind = atlasPath.lastIndexOf("/");
-            if(ind != -1)
-                texturePath = atlasPath[0..ind+1]~ atlasTexturePath;
-            else
-                texturePath = atlasTexturePath;
-        }
-        auto t = new Texture();
-        ErrorHandler.assertErrorMessage(t.load(texturePath), "BitmapFontError", "Could not load texture at path "~texturePath);
-        texture = t;
-
-        for(int i = 0; i < characters.length; i++)
-        {
-            auto ch = &characters[i];
-            if(ch.id != 0)
-            {
-                ch.normalizedX = cast(float)ch.x/texture.width;
-                ch.normalizedY = cast(float)ch.y/texture.height;
-                ch.normalizedWidth = cast(float)ch.width/texture.width;
-                ch.normalizedHeight = cast(float)ch.height/texture.height;
-            }
-        }
-    }
-
-
-    static HipBitmapFont fromFile(string atlasPath, string texturePath = "")
-    {
-        auto ret = new HipBitmapFont();
-        ret.readAtlas(atlasPath);
-        ret.readTexture(texturePath);
-        return ret;
-    }
-    
-    override int getKerning(dchar current, dchar next)
-    {
-        HipCharKerning* chKerning = current in kerning;
-        if(chKerning is null)
-            return 0;
-        int* kerningValue = next in (*chKerning);
-        if(kerningValue is null)
-            return 0;
-        return *kerningValue;
-    }
-    
-
-}
 
 
 private Shader bmTextShader = null;
 
-class HipBitmapText
+class HipTextRenderer
 {
     HipFont font;
     Mesh mesh;
