6411c86350e1f10acd6c70af6ae98a81d3f61c48
Marcelo Silva Nascimento Mancini <msnmancini@hotmail.com>
2022-07-13 14:09:54 -0300

    Separating file systems per system
    Former-commit-id: 63a4d8e51eb63689e4327914b7c4016a7916b5aa
    Former-commit-id: 6c1ba42f03d04910e0d382ff5efa787902afef49 [formerly e1f30db47b9f4af5d7a6f9dd7b698456009e1fe7]
    Former-commit-id: 8b3b410b2e274ae6a305bff4421fae464f49a6ef

---
+++ modules/filesystem/source/hip/filesystem/systems/android.d
@@ -0,0 +1,94 @@
+module hip.filesystem.systems.android;
+import hip.hipengine.api.filesystem.hipfs;
+
+version(Android)
+{
+    public import hip.jni.android.asset_manager;
+    public import hip.jni.android.asset_manager_jni;
+    AAssetManager* aaMgr;
+    class HipAndroidFile : HipFile
+    {
+        import core.stdc.stdio;
+        AAsset* asset;
+        @disable this();
+        this(string path, FileMode mode)
+        {
+            super(path, mode);
+        }
+        override ulong getSize()
+        {
+            ErrorHandler.assertErrorMessage(asset != null, "HipAndroidFile error",
+            "Can't get size from null asset '"~path~"'");
+            return cast(ulong)AAsset_getLength64(asset);
+        }
+        override bool open(string path, FileMode mode)
+        {
+            asset = AAssetManager_open(aaMgr, path.toStringz, AASSET_MODE_BUFFER);
+            return asset != null;
+        }
+        override int read(void* buffer, ulong count)
+        {
+            ErrorHandler.assertErrorMessage(asset != null, "HipAndroidFile error", "Can't read null asset");
+            return AAsset_read(asset, buffer, count);
+        }
+        override long seek(long count, int whence = SEEK_CUR)
+        {
+            ErrorHandler.assertErrorMessage(asset != null, "HipAndroidFile error", "Can't seek null asset");
+            super.seek(count, whence);
+            version(offset64)
+                return AAsset_seek64(asset, count, SEEK_CUR);
+            else
+                return AAsset_seek(asset, cast(int)count, SEEK_CUR);
+        }
+        bool write(string path, void[] data)
+        {
+            std.file.write(path, data);
+            return true;
+        }
+        void close()
+        {
+            if(asset != null)
+                AAsset_close(asset);
+        }
+    }
+
+    class HipAndroidFileSystemInteraction : IHipFileSystemInteraction
+    {
+        bool read(string path, out void[] output)
+        {
+            HipAndroidFile f = new HipAndroidFile(path, FileMode.READ);
+            output.length = f.size;
+            bool ret = f.read(output.ptr, f.size) >= 0;
+            f.close();
+            destroy(f);
+            return ret;
+        }
+        bool write(string path, void[] data)
+        {
+            HipAndroidFile f = new HipAndroidFile(path, FileMode.WRITE);
+            bool ret = f.write(path, data);
+            f.close();
+            destroy(f);
+            return ret;
+        }
+        bool exists(string path)
+        {
+            HipAndroidFile f = new HipAndroidFile(path, FileMode.WRITE);
+            bool x = f.asset != null;
+            f.close();
+            destroy(f);
+            return x;
+        }
+        bool remove(string path)
+        {
+            std.file.remove(path);
+            return true;
+        }
+        
+        bool isDir(string path)
+        {
+            return false; 
+        }
+        
+    }
+}
---
+++ modules/filesystem/source/hip/filesystem/systems/uwp.d
@@ -0,0 +1,150 @@
+module hip.filesystem.systems.uwp;
+import hip.hipengine.api.filesystem.hipfs;
+
+version(UWP)
+{
+    import core.sys.windows.windef;
+    import hip.bind.external : UWPCreateFileFromAppW,
+                        UWPDeleteFileFromAppW,
+                        UWPGetFileAttributesExFromAppW;
+    class HipUWPFile : HipFile
+    {
+        HANDLE fp;
+        import std.utf:toUTF16z;
+        @disable this();
+        this(string path, FileMode mode)
+        {
+            super(path, mode);
+        }
+        bool open(string path, FileMode mode)
+        {
+            DWORD desiredAcces;
+            DWORD shareMode;
+            DWORD createDisposition;
+            switch(mode)
+            {
+                case FileMode.READ:
+                    desiredAcces = GENERIC_READ;
+                    shareMode = FILE_SHARE_READ;
+                    createDisposition = OPEN_EXISTING;
+                    break;
+                case FileMode.WRITE:
+                    desiredAcces = GENERIC_WRITE;
+                    shareMode = FILE_SHARE_WRITE;
+                    createDisposition = CREATE_ALWAYS;
+                    break;
+                case FileMode.READ_WRITE:
+                    desiredAcces = GENERIC_READ | GENERIC_WRITE;
+                    shareMode = FILE_SHARE_READ | FILE_SHARE_WRITE;
+                    createDisposition = OPEN_ALWAYS;
+                    break;
+                case FileMode.APPEND:
+                case FileMode.READ_APPEND:
+                default:
+                    shareMode = FILE_SHARE_VALID_FLAGS;
+                    desiredAcces = GENERIC_ALL;
+                    createDisposition = OPEN_EXISTING;
+            }
+            
+            fp = UWPCreateFileFromAppW(path.toUTF16z, desiredAcces, shareMode, cast(SECURITY_ATTRIBUTES*)null, createDisposition, 
+            FILE_ATTRIBUTE_NORMAL, cast(void*)null);
+
+            return fp != null;
+        }
+        int read(void* buffer, ulong count)
+        {
+            if(fp == null) return 0;
+
+            uint numRead;
+            if(ReadFile(fp, buffer, cast(int)count, &numRead, cast(OVERLAPPED*)null))
+                return numRead;
+            return 0;
+        }
+        ///Whence is the same from stdio
+        override long seek(long count, int whence)
+        {
+            super.seek(count, whence);
+            if(fp == null) return 0;
+            DWORD winWhence;
+            switch(whence)
+            {
+                case SEEK_CUR:
+                    winWhence = FILE_CURRENT;
+                    break;
+                case SEEK_END:
+                    winWhence = FILE_END;
+                    break;
+                case SEEK_SET:
+                    winWhence = FILE_BEGIN;
+                    break;
+                default:ErrorHandler.assertExit(false, "Unsupported whence");
+            }
+            LARGE_INTEGER ret;
+            LARGE_INTEGER countTemp;
+            countTemp.QuadPart = count;
+            SetFilePointerEx(fp, countTemp, &ret, winWhence);
+            return cast(int)ret.QuadPart;
+        }
+        ulong getSize()
+        {
+            LARGE_INTEGER lg;
+            if(fp == null) return 0;
+
+            if(GetFileSizeEx(fp,  &lg))
+                return cast(ulong)lg.QuadPart;
+            return 0;
+        }
+        void close()
+        {
+            if(fp != null)
+                CloseHandle(fp);
+        }
+    }
+    class HipUWPileSystemInteraction : IHipFileSystemInteraction
+    {
+
+        bool read(string path, out void[] output)
+        {
+            if(ErrorHandler.assertErrorMessage(exists(path), "FileSystem Error:", "Filed named '"~path~"' does not exists"))
+                return false;
+            HipUWPFile f = new HipUWPFile(path, FileMode.READ);
+            import hip.console.log;
+            logln(path, " " , f.getSize);
+            if(f.fp == null) return false;
+            output.length = f.size;
+            f.read(output.ptr, f.size);
+            f.close();
+            destroy(f);
+            return true;
+        }
+        bool write(string path, void[] data)
+        {
+            HipUWPFile f = new HipUWPFile(path, FileMode.WRITE);
+            if(f.fp == null) return false;
+
+            uint bytesWritten;
+            bool ret = cast(bool)WriteFile(f.fp, data.ptr, cast(uint)data.length, &bytesWritten, cast(OVERLAPPED*)null);
+            destroy(f);
+            return ret;
+        }
+        bool exists(string path)
+        {
+            import std.utf:toUTF16z;
+            WIN32_FILE_ATTRIBUTE_DATA info;
+            UWPGetFileAttributesExFromAppW(path.toUTF16z, GET_FILEEX_INFO_LEVELS.GetFileExInfoStandard, &info);
+            return info.dwFileAttributes != INVALID_FILE_ATTRIBUTES;
+        }
+        bool remove(string path)
+        {
+            import std.utf:toUTF16z;
+            if(!exists(path)) return false;
+            return cast(bool)DeleteFile(path.toUTF16z);
+        }
+        
+        bool isDir(string path)
+        {
+            return false;
+        }
+        
+    }
+}
