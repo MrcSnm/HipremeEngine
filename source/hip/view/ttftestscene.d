97f49c5a23c89ea99261c6f74b4fcd8fc3b57bad
Marcelo Silva Nascimento Mancini <msnmancini@hotmail.com>
2022-07-14 08:53:45 -0300

    Update ttftestscene.d
    Former-commit-id: a06c39f941d88bb81ae76456a7991e98fb375a86
    Former-commit-id: b058bb7dfcc8fc98bc8f5403c47dcd35e32d61b3 [formerly b2e6a58088496dcdba0e61ab9804617e45cbe45d]
    Former-commit-id: 1fdf0f984363169030e1bbb16e2c4bc6cd5a5372

--- source/hip/view/ttftestscene.d
+++ source/hip/view/ttftestscene.d
@@ -1,13 +1,44 @@
 module hip.view.ttftestscene;
 
-import hip.view.scene;
 import hip.font.ttf;
+import hip.view.scene;
+import hip.event.handlers.keyboard_layout;
+import hip.hiprenderer;
+import hip.graphics.g2d;
+import hip.graphics.mesh;
+import hip.event.handlers.keyboard;
+import hip.console.log;
+
 
 class TTFTestScene : Scene
 {
-    // TTFFont font;
-    override void init()
+    static KeyboardLayout abnt2;
+    HipTextRenderer txt;
+
+    this()
     {
-        // font = new TTFFont("fonts/arial.ttf");
+        abnt2 = new KeyboardLayoutABNT2();
+        txt = new HipTextRenderer();
+        txt.x = HipRenderer.width/2;
+        txt.alignh = HipTextAlign.CENTER;
+
+        auto fnt = new Hip_TTF_Font("fonts/arial.ttf");
+        ubyte[] texture = fnt.generateTexture(32);
+        fnt.loadTexture;
+        txt.y+= 100;
+        txt.setFont(fnt);
+    }
+
+    override void render()
+    {
+        HipRenderer.setColor(0,0,0,255);
+        HipRenderer.clear(); 
+        // string _txt = txt.text;
+        // string input = KeyboardHandler.getInputText(abnt2);
+        // _txt~= input;
+        // txt.setText(_txt);
+        txt.setText("Hello\nHello\nWorld");
+        txt.render();
+        txt.mesh.shader.setFragmentVar("FragBuf.uColor", cast(float[4])[1.0, 1.0, 0.0, 1.0]);
     }
 }
