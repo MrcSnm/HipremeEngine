4bf9472670e1417775c3ddea75a3fba0cf50c771
Hipreme <msnmancini@hotmail.com>
2023-01-12 13:49:30 -0300

    Added prebuild, fixed memory leaks
    Former-commit-id: ade1c1150b76125adac14a4788e24c66cbb7abfa
    Former-commit-id: b0d9967e01b1990be357b0996a19244acec9cba0 [formerly c4d9716ba4d6c708c0b87f957fc4d52990bd3edf]
    Former-commit-id: 15de3698724fa010fdf400e9e229c5b50c04c9d5

---
+++ build/wasm/prebuild/gendir.d
@@ -0,0 +1,48 @@
+import std.stdio;
+import std.path:relativePath, buildPath, absolutePath;
+import std.conv:to;
+import std.array:appender;
+import std.file:dirEntries, DirEntry, SpanMode, exists, write;
+import core.stdc.stdlib;
+
+enum OUTPUT_NAME = "directories.json";
+
+///Hipreme Engine that generates a JSON containing the assets directory.
+int main(string[] args)
+{
+    if(args.length < 3)
+    {
+        writeln("Usage: rdmd gendir.d inputDirectory outputDir");
+        return EXIT_FAILURE;
+    }
+    auto output = appender!string;
+    
+    genJson(absolutePath(args[1]), output);
+
+    if(exists(args[2]))
+        write(buildPath(args[2], OUTPUT_NAME), output[]);
+
+    return EXIT_SUCCESS;
+}
+
+void genJson(T)(string startDir, ref T output)
+{
+    bool first = true;
+    output~= "{";
+    foreach(DirEntry e; dirEntries(startDir, SpanMode.shallow))
+    {
+        if(!first)
+            output~=",";
+        first = false;
+        output~= '"';
+        output~= e.name.relativePath(startDir);
+        output~= `": `;
+        if(e.isDir)
+            genJson(e.name, output);
+        else
+        {
+            output~= e.size.to!string;
+        }
+    }
+    output~= "}";
+};
