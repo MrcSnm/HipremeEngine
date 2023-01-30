61db4d093302c22dcc0fb6c53430392c8fc5fd34
Hipreme <msnmancini@hotmail.com>
2023-01-30 10:16:08 -0300

    Adding tools dir
    Former-commit-id: 320504829420ca49984b0786f229ba4035c64edd
    Former-commit-id: 7133d3b5dd305bacf74aa160dd7b8704d934affb [formerly b87f51a63a20a5676c12ad71fe565e617e2e5d4b]
    Former-commit-id: 8681121a9c8c84fee6b2d1dcc4277d5221d6a04a

--- wasmbuild.bat
+++ wasmbuild.bat
@@ -1,4 +1,4 @@
-rdmd build/gendir.d assets/defaults build/wasm/generated
+rdmd tools/build/gendir.d assets/defaults build/wasm/generated
 
 set DFLAGS=-I=%HIPREME_ENGINE%/modules/d_std/source ^
 -I=%HIPREME_ENGINE%/build/wasm/runtime/webassembly/arsd-webassembly ^
