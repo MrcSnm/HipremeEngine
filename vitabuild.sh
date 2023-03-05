bbdcea6284699ef8ccc35972a0e752ee40058ae5
Hipreme <msnmancini@hotmail.com>
2023-03-05 09:38:26 -0300

    Fixed vita builder having DFLAGS when running.

--- vitabuild.sh
+++ vitabuild.sh
@@ -3,7 +3,7 @@ export DFLAGS="-I=$HIPREME_ENGINE/modules/d_std/source \
 -d-version=PSVita \
 -d-version=PSV \
 -preview=shortenedMethods \
--mtriple=armv7a-unknown-newlib \
+-mtriple=armv7a-unknown-unknown \
 --revert=dtorfields \
 -mcpu=cortex-a9 \
 -O0 \
@@ -12,6 +12,7 @@ export DFLAGS="-I=$HIPREME_ENGINE/modules/d_std/source \
 --relocation-model=static \
 -d-version=CarelessAlocation"
 
-dub -c psvita-main --compiler=ldc2 --arch=armv7a-unknown-newlib
-dub -c psvita --compiler=ldc2 --arch=armv7a-unknown-newlib
+dub -c psvita-main --compiler=ldc2 --arch=armv7a-unknown-unknown
+dub -c psvita --compiler=ldc2 --arch=armv7a-unknown-unknown
+export DFLAGS="";
 rdmd tools/build/vita.d
