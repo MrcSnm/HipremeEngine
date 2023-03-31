d1ff07c47dc82dba5dd1e3528bbc05d24a77689d
MrcSnm <msnmancini@hotmail.com>
2023-03-31 00:52:25 -0300

    Now appleosbuild will run the application
    Former-commit-id: 1540b4b0b9a3b59b0bc2ed8d35411583e70a82bc
    Former-commit-id: 5feb7496b3671b54cc7bba5cf7eef669d057529e [formerly 44c2c28056a32a4440a0646b3e9dbbb4b66e6982]
    Former-commit-id: 541d3688baa22dd1222e89e47dcb0c16aaeb9328

--- tools/build/scripts/appleosbuild.sh
+++ tools/build/scripts/appleosbuild.sh
@@ -3,3 +3,5 @@ rm -r build/appleos/HipremeEngine\ D/libs
 dub -c appleos
 dub -c appleos-main
 rdmd tools/build/copylinkerfiles.d "-c appleos" "build/appleos/HipremeEngine D/libs"
+cd build/appleos/
+xcodebuild -jobs 8 -configuration Debug -scheme HipremeEngine\ macOS build CONFIGURATION_BUILD_DIR="bin" && cd bin && open -a "HipremeEngine"
