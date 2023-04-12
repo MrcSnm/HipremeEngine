6d6f8561d53f825be96288b681c9f07dc0f053ae
Hipreme <msnmancini@hotmail.com>
2023-04-12 18:58:58 -0300

    Fixed ldc specific flags not being filtered, fixed hiper on linux
    Former-commit-id: bf3ef3049923ae3c461aa01cc25b06e5b7223632
    Former-commit-id: 68d700915d3c1526a07ca7db7dcde5e269848c7f [formerly 2385f5607b4e6b4ec268a0f274bd41cfc355472f]
    Former-commit-id: 9cb03f90afba67c41a61c741b182f2ba27e78611

--- tools/user/hiper/source/app.d
+++ tools/user/hiper/source/app.d
@@ -19,42 +19,8 @@ bool isProjectNameValid(string name)
 	return name != "" && name != "hiper";
 }
 
-int main(string[] args)
+int onPathSelected(string path)
 {
-	import std.process;
-	if(!("HIPREME_ENGINE" in environment))
-	{
-		writeln("Please setup HIPREME_ENGINE environment variable to hiper being able to correctly generate your project");
-		return 1;
-	}
-
-	string path;
-
-	if(args.length < 2)
-	{
-		bool hasCancel = false;
-		while(path == null && !hasCancel)
-		{
-			getSaveFileName((string folderName)
-			{
-				if(folderName.hasSpace)
-				{
-					messageBox("Save Project Error", "Your project name should not contain spaces", MessageBoxStyle.OK, MessageBoxIcon.Error);
-					return;
-				}
-				path = folderName;
-				if(path[$-1] == '\0')
-					path = path[0..$-1];
-			}, "Name of your project(Should not contain spaces)", ["HipremeProject"], (){hasCancel = true;});
-		}
-
-	}
-	else
-	{
-		writeln("Getting the path from argument ", args[1]);
-		path = args[1];
-	}
-
 	if(path == "")
 	{
 		writeln("No folder selected");
@@ -71,5 +37,41 @@ int main(string[] args)
 
 	string projName = pathSplitter(path).array[$-1];
 	generateProject(path, DubProjectInfo("HipremeEngine", projName), TemplateInfo());
+	return 0;
+}
+
+void popupForProjectName()
+{
+	getSaveFileName((string folderName)
+	{
+		if(folderName.hasSpace)
+		{
+			messageBox("Save Project Error", "Your project name should not contain spaces", MessageBoxStyle.OK, MessageBoxIcon.Error);
+			popupForProjectName();
+		}
+		else
+		{
+			if(folderName[$-1] == '\0')
+				folderName = folderName[0..$-1];
+			onPathSelected(folderName);
+		}
+	}, "Name of your project(Should not contain spaces)", ["HipremeProject"]);
+}
+
+int main(string[] args)
+{
+	import std.process;
+	if(!("HIPREME_ENGINE" in environment))
+	{
+		writeln("Please setup HIPREME_ENGINE environment variable to hiper being able to correctly generate your project");
+		return 1;
+	}
+	if(args.length < 2)
+		popupForProjectName();
+	else
+	{
+		writeln("Getting the path from argument ", args[1]);
+		onPathSelected(args[1]);
+	}
 	return 0;
 }
