31674c00b550b99e5ab62924f92eac4afad73462
Hipreme <msnmancini@hotmail.com>
2023-04-25 21:37:38 -0300

    Clean Windows now can build with no dependency!

--- tools/user/build_selector/source/targets/linux.d
+++ tools/user/build_selector/source/targets/linux.d
@@ -11,7 +11,7 @@ void prepareLinux(Choice* c, ref Terminal t, ref RealTimeConsoleInput input, in 
 		wait(spawnShell("sudo apt-get install libgl1-mesa-dev"));
 	}
 	std.file.chdir(configs["gamePath"].str);
-	waitDub(t, "-c script "~cOpts.getDubOptions, "");
+	waitDub(t, "build -c script "~cOpts.getDubOptions, "");
 	std.file.chdir(configs["hipremeEnginePath"].str);
 	waitDub(t, "-c script "~cOpts.getDubOptions ~ " -- "~configs["gamePath"].str, "", true);
 
--- tools/user/build_selector/source/targets/windows.d
+++ tools/user/build_selector/source/targets/windows.d
@@ -4,7 +4,7 @@ import commons;
 void prepareWindows(Choice* c, ref Terminal t, ref RealTimeConsoleInput input, in CompilationOptions cOpts)
 {
     std.file.chdir(configs["gamePath"].str);
-	waitDub(t, "-c script "~cOpts.getDubOptions, "");
+	waitDub(t, "build -c script "~cOpts.getDubOptions, "");
 	std.file.chdir(configs["hipremeEnginePath"].str);
 	waitDub(t, "-c script "~cOpts.getDubOptions ~ " -- "~configs["gamePath"].str, "", true);
 }
