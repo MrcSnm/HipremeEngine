1e745ea70ccee264e8faf212800dc480a835862f
Hipreme <msnmancini@hotmail.com>
2023-12-13 23:22:57 -0300

    Fixed: iOS building faster since no need to codesign. Build selector now will clear cache when changing the game. Fixed wrong touch coordinates

--- tools/user/build_selector/source/targets/ios.d
+++ tools/user/build_selector/source/targets/ios.d
@@ -3,16 +3,24 @@ import common_macos;
 import commons;
 import global_opts;
 
+
+enum TARGET_TYPE = "simulator";
 enum iosArch =
 [
 	"simulator" : "x86_64",
 	"hardware"  : "arm64"
 ];
 
+string getExtraCommand(string type)
+{
+	if(type == "simulator") return " -sdk iphonesimulator ";
+	return "";
+}
+
 ChoiceResult prepareiOS(Choice* c, ref Terminal t, ref RealTimeConsoleInput input, in CompilationOptions cOpts)
 {
 	string buildTarget = getBuildTarget("ios");
-	string arch = iosArch["simulator"];
+	string arch = iosArch[TARGET_TYPE];
 	prepareAppleOSBase(c,t,input);
 
 	string out_extraLinkerFlags;
@@ -30,6 +38,7 @@ ChoiceResult prepareiOS(Choice* c, ref Terminal t, ref RealTimeConsoleInput inpu
 
 	cached(() => timed(() => outputTemplateForTarget(t, buildTarget)));
 	string codeSignCommand = getCodeSignCommand(t);
+	string extraCommands = getExtraCommand(TARGET_TYPE);
 
 	with(WorkingDir(getHipPath))
 	{
@@ -56,7 +65,7 @@ ChoiceResult prepareiOS(Choice* c, ref Terminal t, ref RealTimeConsoleInput inpu
 				"xcodebuild -jobs 8 -configuration Debug -scheme 'HipremeEngine iOS' " ~
 				clean ~
 				"build CONFIGURATION_BUILD_DIR=\"bin\" "~ 
-				codeSignCommand ~
+				codeSignCommand ~ extraCommands ~
 				"-destination 'platform=iOS Simulator,name=iPhone 14,OS=16.2' " ~
 				" && cd bin && HipremeEngine.app/Contents/iOS/HipremeEngine")
 			);
