3e210a4577b0e2d0ac89f04f5dcfc440da13129d
Hipreme <msnmancini@hotmail.com>
2023-12-30 23:25:11 -0300

    WIP: apigen.d now is at least workable

--- tools/build/apigen.d
+++ tools/build/apigen.d
@@ -2,8 +2,11 @@ module tools.build.apigen;
 static import std.file;
 import std.stdio;
 import std.array;
+import std.string:indexOf;
 import std.traits;
+import std.path;
 import std.process;
+import std.algorithm.sorting;
 import std.algorithm.searching;
 import std.algorithm.mutation;
 
@@ -11,15 +14,16 @@ import std.algorithm.mutation;
 version(SecondRun){}
 else version = FirstRun;
 
-version(DigitalMars)
+enum TheVersion = "-version=";
+enum StrImport = "-J=";
+enum MV = "-mv=";
+
+
+string[] unique(string[] input)
 {
-    enum TheVersion = "-version=";
-    enum StrImport = "\"-J=";
-} 
-else version(LDC)
-{
-    enum TheVersion = "--d-version=";
-    enum StrImport = "-J";
+    bool[string] ret;
+    foreach(i; input) ret[i] = true;
+    return ret.keys.dup;
 }
 
 /** 
@@ -33,37 +37,64 @@ string[] getImportPaths(string enginePath)
     string currDir = std.file.getcwd();
     std.file.chdir(enginePath);
     scope(exit) std.file.chdir(currDir);
-    auto exec = executeShell("dub describe --data=import-paths --vquiet");
+    auto exec = executeShell("dub describe --data=import-paths --data=dflags --vquiet");
     if(exec.status)
     {
         writeln("Error executing dub on path ", enginePath, ": ", exec.output);
         return [];
     }
-    string[] importsTemp = split(exec.output.replace("\"", "").replace("\r", "").replace("\n", ""), " ");
-    string[] rms = ["--oq", "-od="];
-    for(int i = 0; i < importsTemp.length; i++)
+    static string[] separeMvFlags(ref string[] input)
     {
-        for(int iRm = 0; iRm < rms.length; i++)
+        string[] ret;
+        for(int i = 0; i < input.length; i++)
         {
-            auto res = findSplit(importsTemp[i], rms[iRm]);
+            auto res = findSplit(input[i], "-mv=");
             bool found = res[1].length != 0;
             if(found)
             {
-                remove(importsTemp, i);
-                rms = rms[1..$];
+                ret~= MV~res[2];
+                input = input[0..i] ~ input[i+1..$];
+                i--;
+            }
+        }
+        return ret;
+    }
+    string[] importsTemp = split(exec.output.replace("\"", "").replace("\r", "").replace("\n", ""), " ");
+    importsTemp = unique(importsTemp);
+    string[] mvFlags = separeMvFlags(importsTemp);
+    string[] rms = ["--oq", "-od=", "-mixin", "--disable-verify"];
+    for(int i = 0; i < importsTemp.length; i++)
+    {
+        for(int iRm = 0; iRm < rms.length; iRm++)
+        {
+            if(indexOf(importsTemp[i], rms[iRm]) != -1)
+            {
+                importsTemp = importsTemp[0..i] ~ importsTemp[i+1..$];
+                rms = rms[0..iRm] ~ rms[iRm+1..$];
                 i--;
                 break;
             }
         }
     }
-    return importsTemp;
+    
+    foreach(i; 0..importsTemp.length)
+    {
+        long index = importsTemp[i].indexOf("-J");
+        if(index != -1)
+        {
+            if(!isAbsolute(importsTemp[i]["-J".length..$]))
+                importsTemp[i] = "-J"~ enginePath ~ importsTemp[i]["-J".length..$];
+        }
+    }
+    return mvFlags ~ importsTemp;
 }
 
 version(FirstRun)
-int FirstRunMain(string[] args)
+int main(string[] args)
 {
     if(args.length < 3)
     {
+        writeln("Received ", args[1..$], " expected: ");
         writeln("Usage: rdmd apigen.d <hipremeEnginePath> <root>");
         return 1;
     }
@@ -71,49 +102,69 @@ int FirstRunMain(string[] args)
     std.file.write("args.txt", filesToProcess);
 
     string[] versions = ["", "SecondRun"];
-    string cmd = "rdmd "~
-        versions.join(" "~TheVersion)~
-        " \""~StrImport~".\""~
-        " \"-i\" "~
-        " \""~getImportPaths(args[1]).join("\" \"")~"\" apigen ";
-    writeln(cmd);
 
-    return wait(spawnShell(cmd));
+    string rdmdArgs = versions.join(" "~TheVersion)~ " "~
+        StrImport~". "~
+        getImportPaths(args[1]).join(" ")~" apigen.d";
+
+    writeln(rdmdArgs);
+
+    string ext = "";
+    version(Windows) ext = ".exe";
+    
+    return wait(spawnShell("dmd "~rdmdArgs~" && apigen"~ext));
 }
+
+
+string getModuleInfoVarName(string moduleName)
+{
+    import std.conv;
+    char[256] ret = "_D";
+    size_t length = 2;
+    foreach(txt; moduleName.split("."))
+    {
+        string packageLength = to!string(txt.length);
+        size_t newSize = length + txt.length+ packageLength.length;
+        ret[length..length+packageLength.length] = packageLength;
+        ret[length+packageLength.length..newSize] = txt;
+        length = newSize;
+    }
+    enum moduleInfoStr = "12__ModuleInfoZ";
+    ret[length..length+moduleInfoStr.length] = moduleInfoStr;
+    return ret.idup[0..length+moduleInfoStr.length];
+}
+
+
 
 version(SecondRun)
-int SecondRunMain(string[] args)
 {
-    import hip.util.reflection;
-    writeln("Second Run");
-    enum string[] filesToProcess = import("args.txt").split("\n");
-    static foreach(mod; filesToProcess)
+    //Hack since D expects a module info to be linked on the imported files...
+    enum string[] ApiGenArgs = import("args.txt").split("\n");
+    static foreach(a; ApiGenArgs)
     {
-        mixin("import ", mod,";");
-        static foreach(member; __traits(allMembers, mixin(mod)))
-        {{
-            alias mem = __traits(getMember, mixin(mod), member);
-            ModuleImplementor mi;
-            static if(mem.hasUDA!ModuleImplementor && isFunction!mem)
-            {
-                mi = mem.getUDAs!(ModuleImplementor)[0];
-                pragma(msg, ReturnType!mem, " ", mi.apiOutput, "_", member, " (", Parameters!mem, ")");
-
-            }
-        }}
+        extern(C) mixin("void* ", getModuleInfoVarName(a), ";");
     }
-
-    return 0;
-}
-
-int main(string[] args)
-{
-    version(FirstRun)
+    
+    import hip.util.reflection:ModuleImplementor;
+    int main(string[] args)
     {
-        return FirstRunMain(args);
-    }
-    else    
-    {
-        return SecondRunMain(args);
+        enum string[] filesToProcess = ApiGenArgs;
+        static foreach(mod; filesToProcess)
+        {
+            mixin("import ", mod,";");
+            static foreach(member; __traits(allMembers, mixin(mod)))
+            {{
+                alias mem = __traits(getMember, mixin(mod), member);
+
+                static if(hasUDA!(mem, ModuleImplementor) && isFunction!mem)
+                {{
+                    enum ModuleImplementor mi = __traits(getAttributes, mem)[0];
+                    pragma(msg, ReturnType!mem, " ", mi.apiOutput, "_", member, Parameters!mem);
+
+                }}
+            }}
+        }
+
+        return 0;
     }
 }
