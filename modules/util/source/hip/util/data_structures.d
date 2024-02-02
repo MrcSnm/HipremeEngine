6c9ffb446810e70a48fa06250ba255b1201659d1
Hipreme <msnmancini@hotmail.com>
2024-02-01 22:33:46 -0300

    Removed: Concurrency from util
    Former-commit-id: 672a9394fa89ac2889cf156569fd96315f59f54b
    Former-commit-id: 6623dadee6583b1f7885876620df00b102311320 [formerly 1738ae6a59fee1821897d24d492f9aeed83f5984]
    Former-commit-id: 401162f8d4674f4d979ac2dfcbcdb51282113f2b

--- modules/util/source/hip/util/data_structures.d
+++ modules/util/source/hip/util/data_structures.d
@@ -653,60 +653,6 @@ struct Map(K, V)
 }
 alias AArray = Map;
 
-struct RingBuffer(T, uint Length)
-{
-    import hip.util.concurrency:Volatile;
-    @nogc:
-
-    T[Length] data;
-    private Volatile!uint writeCursor;
-    private Volatile!uint readCursor;
-
-    this()
-    {
-        this.writeCursor = 0;
-        this.readCursor = 0;
-    }
-
-    void push(T data)
-    {
-        this.data[writeCursor] = data;
-        writeCursor = (writeCursor+1) % Length;
-    }
-    ///It may read less than count if it is out of bounds
-    immutable T[] read(uint count)
-    {
-        uint temp = readCursor;
-        if(temp + count > Length)
-        {
-            readCursor = 0;
-            return data[temp..Length];
-        }
-        readCursor = (temp+count)%Length;
-        return data[temp .. count];
-    }
-
-    immutable T read()
-    {
-        uint temp = readCursor;
-        immutable T ret = data[temp];
-        readCursor = (temp+1)%Length;
-        return ret;
-    }
-
-    void dispose()
-    {
-        data = null;
-        length = 0;
-        writeCursor = 0;
-        readCursor = 0;
-    }
-
-    ~this()
-    {
-        dispose();
-    }
-}
 
 /**
 *   High efficient(at least memory-wise), tightly packed Input queue that supports any kind of data in
