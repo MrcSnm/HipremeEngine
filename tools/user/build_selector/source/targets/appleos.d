1423043d57c66734e4b53209cd1c5c812daed35e
Hipreme <msnmancini@hotmail.com>
2024-03-30 11:24:07 -0300

    Fixed: Build for appleos
    Former-commit-id: 9fa60e78e7e3359a1187e1ab71476ddc461f68aa
    Former-commit-id: 422ee1c9cae18cb9e75a56fdeea68eb752bb0e5c [formerly f7754990f0a10f661f87008ecc0d48d7ffb5aed8]
    Former-commit-id: 6e5b812203a0800cb3a3c0645a9c6ccd70659d59

--- tools/user/build_selector/source/targets/appleos.d
+++ tools/user/build_selector/source/targets/appleos.d
@@ -3,13 +3,19 @@ import commons;
 import common_macos;
 import global_opts;
 
+version(ARM) enum isARM = true;
+else version(AArch64) enum isARM = true;
+else enum isARM = false;
+
+
+enum archFolder = isARM ? "arm64" : "x86_64";
 
 ChoiceResult prepareAppleOS(Choice* c, ref Terminal t, ref RealTimeConsoleInput input, in CompilationOptions cOpts)
 {
 	prepareAppleOSBase(c,t,input);
 
 	string out_extraLinkerFlags;
-	setupPerCompiler(t, getSelectedCompiler, "x86_64", out_extraLinkerFlags);
+	setupPerCompiler(t, getSelectedCompiler, archFolder, out_extraLinkerFlags);
 	cached(() => timed(() => outputTemplateForTarget(t)));
 	string codeSignCommand = getCodeSignCommand(t);
 	with(WorkingDir(getHipPath))
